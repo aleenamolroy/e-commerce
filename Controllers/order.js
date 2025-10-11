fe6bf43e03c5000f50e89767cab49455646c1c72
aleenamolroy <aleenamolroy02@gmail.com>
2025-10-11 20:04:36 +0530

    commit order changes

--- Controllers/order.js
+++ Controllers/order.js
@@ -2,6 +2,8 @@ import Order from "../Models/order.js";
 import Cart from "../Models/cart.js";
 import mongoose, { Aggregate } from "mongoose";
 import Category from "../Models/category.js";
+import { log } from "console";
+import Product from "../Models/product.js";
 export const createOrder = async (req, res) => {
     try {
         const userId = new mongoose.Types.ObjectId(req.session.user.id)
@@ -33,7 +35,9 @@ export const createOrder = async (req, res) => {
 
 export const viewOrder = async (req, res) => {
     try {
+
         const { id } = req.params
+
         const userId = new mongoose.Types.ObjectId(req.session.user.id)
         // if (!mongoose.Types.ObjectId.isValid(id) || !mongoose.Types.ObjectId.isValid(req.session.user.id)) {
         //     return res.status(400).json({ message: 'Invalid order or user ID' });
@@ -75,7 +79,6 @@ export const viewOrder = async (req, res) => {
                 }
             }
         ])
-        console.log(order);
 
         if (!order || order.length === 0) {
             return res.status(400).json({ message: 'order not found' })
@@ -88,7 +91,72 @@ export const viewOrder = async (req, res) => {
 
     }
 }
+export const showorder = async (req, res) => {
+    try {
+        const userId = new mongoose.Types.ObjectId(req.session.user.id)
+        const order = await Order.aggregate([
+            { $match: { user: userId } },
+            {
+                $addFields: {
+                    totalPriceOriginal: "$totalPrice",
+                    shippingStatus: "$shippingStatus",
+                    paymentStatus: "$paymentStatus",
+                    createdAt: "$createdAt"
+                },
+            },
+            { $unwind: "$items" },
+            {
+                $lookup: {
+                    from: "products",
+                    localField: "items.product",
+                    foreignField: "_id",
+                    as: "productDetails"
+                }
+            },
+            { $unwind: "$productDetails" },
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "productDetails.category",
+                    foreignField: "_id",
+                    as: "categoryDetails"
+                }
+            },
+            { $unwind: "$categoryDetails" },
+            {
+                $group: {
+                    _id: "$_id",
+                    user: { $first: "$user" },
+                    items: {
+                        $push: {
+                            product: {
+                                id: "$productDetails._id",
+                                name: "$productDetails.name",
+                                price: "$productDetails.price",
+                                productimg: "$productDetails.productimg"
+                            },
+                            category: {
+                                name: "$categoryDetails.name"
+                            },
+                            quantity: "$items.quantity",
+                            subtotal: { $multiply: ["$items.quantity", "$productDetails.price"] }
+                        }
+                    },
+                    shippingStatus: { $first: "$shippingStatus" },
+                    paymentStatus: { $first: "$paymentStatus" },
+                    totalPrice: { $first: "$totalPriceOriginal" },
+                    createdAt: { $first: "$createdAt" }
+                }
+            }
+        ])
+        return res.status(200).json(order)
+    }
+    catch (err) {
+        console.log(err);
 
+        return res.status(500).json({ message: "Internal server error" })
+    }
+}
 export const viewOrders_admin = async (req, res) => {
     try {
         const orders = await Order.aggregate([
@@ -124,7 +192,6 @@ export const viewOrders_admin = async (req, res) => {
             },
             { $sort: { _id: 1 } }
         ])
-        console.log(orders);
 
         if (!orders || orders.length === 0) {
             return res.status(400).json({ message: 'order not found' })
@@ -142,8 +209,8 @@ export const update_status = async (req, res) => {
     try {
         const { id } = req.params
         const { paymentStatus, shippingStatus } = req.body
-        const order= await Order.findById(id)
-        if(!order) return res.status(404).json({message:"Order not found"})
+        const order = await Order.findById(id)
+        if (!order) return res.status(404).json({ message: "Order not found" })
         const validPaymentTransitions = {
             pending: ["paid", "failed"],
             paid: [],
@@ -163,8 +230,8 @@ export const update_status = async (req, res) => {
         const validShippingTransitions = {
             pending: ["shipped", "cancelled"],
             shipped: ["delivered", "cancelled"],
-            delivered: [], 
-            cancelled: [], 
+            delivered: [],
+            cancelled: [],
         };
         if (
             shippingStatus &&
@@ -195,7 +262,7 @@ export const update_status = async (req, res) => {
 export const cancel_order = async (req, res) => {
     try {
         const { id } = req.params
-        const order = await Order.findById(id )
+        const order = await Order.findById(id)
         const { shippingStatus } = req.body
         console.log(req.body)
         if (!order) {
@@ -209,6 +276,25 @@ export const cancel_order = async (req, res) => {
     }
     catch (err) {
         console.log(err);
+        return res.status(500).json({ message: 'server error' })
+
+    }
+}
+export const CancelOrder = async (req, res) => {
+    try {
+        const { id } = req.params
+        const order = await Order.findById(id)
+        const { shippingStatus } = req.body
+        if (order.shippingStatus === 'delivered' || order.shippingStatus === 'cancelled') {
+            return res.status(400).json({ message: "You can't cancel this order please check your order status" })
+        }
+        const orderUpdate = await Order.findByIdAndUpdate(id, { shippingStatus }, { new: true })
+        await orderUpdate.save()
+        return res.status(200).json({ message: "order cancelled" })
+
+    } catch (err) {
+                console.log(err);
+
         return res.status(500).json({ message: 'server error' })
 
     }
--- Routers/order.js
+++ Routers/order.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { cancel_order, createOrder, update_status, viewOrder, viewOrders_admin } from "../Controllers/order.js";
+import { cancel_order, CancelOrder, createOrder, showorder, update_status, viewOrder, viewOrders_admin } from "../Controllers/order.js";
 import { isUser } from "../Middleware/userauth.js";
 import { isAdmin } from "../Middleware/adminauth.js";
 const router = Router()
 
 router.post('/createOrder',isUser,createOrder)
 router.get('/vieworder/:id',isUser,viewOrder)
+router.get('/showorder',isUser,showorder)
+router.delete('/cancelorder/:id',isUser,CancelOrder)
 router.get('/viewOrders',isAdmin,viewOrders_admin)
 router.put('/update/:id',isAdmin,update_status)
 router.delete('/cancel/:id',isAdmin,cancel_order)
