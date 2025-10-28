df0b915da5c6b111da506c1d2f942cd96ca11675
aleenamolroy <aleenamolroy02@gmail.com>
2025-10-28 15:02:26 +0530

    upload change in user Routers

--- Routers/User.js
+++ Routers/User.js
@@ -5,7 +5,7 @@ import { isUser } from "../Middleware/userauth.js";
 const router= Router()
 const storage=multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,"uploads")
+        cb(null,"api/uploads")
     },
     filename:(req,file,cb)=>{
         cb(null,Date.now()+'_'+file.originalname)
